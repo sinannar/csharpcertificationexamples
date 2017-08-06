2ee49c78cc8d04acb5758776afb42fdfc431580a
Sinan NAR <sinan.nar@outlook.com>
2017-08-06 20:23:30 +1200

    async example has been implemented

--- Listing-1-18/Program.cs
+++ Listing-1-18/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Listing_1_18
 {
     class Program
     {
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+
         static void Main(string[] args)
         {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
