6183d24fe2604e45dbb74c62418e2fe7989fe91b
Sinan NAR <sinan.nar@gmail.com>
2017-08-14 20:44:41 +1200

    Listing 1-19 has been added, but its not really usab

---
+++ Listing-1-19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Listing_1_19
+{
+    class Program
+    {
+        public static async Task SleepAsyncA(int milliseconds)
+        {
+            await Task.Run(() => Thread.Sleep(milliseconds));
+            Console.WriteLine("AAA");
+
+        }
+
+        public static Task SleepAsyncB(int milliseconds)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(milliseconds, -1);
+            return tcs.Task;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            SleepAsyncA(1000);
+
+            Console.WriteLine("AAA");
+            Console.ReadKey();
+        }
+    }
+}
