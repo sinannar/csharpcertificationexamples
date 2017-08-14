cf42042901425376452e8314dce09018789b5498
Sinan NAR <sinan.nar@outlook.com>
2017-08-14 21:29:25 +1200

    Concurrent Bag examples has been implemented

---
+++ Listing-1-30/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_1_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+            bag.Add(2);
+            bag.Add(4);
+
+            int result;
+            if (bag.TryTake(out result))
+                Console.WriteLine(result);
+            if (bag.TryTake(out result))
+                Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Listing-1-31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Listing_1_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+            Task.Run(() =>
+            {
+                bag.Add(4);
+                Thread.Sleep(1000);
+                bag.Add(7);
+            });
+
+            Task.Run(() =>
+            {
+                foreach (var item in bag)
+                {
+                    Console.WriteLine(item);
+                }
+            }).Wait();
+            Console.ReadKey();
+        }
+    }
+}
