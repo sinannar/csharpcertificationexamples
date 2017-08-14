52564f37f1985ae9c28a09b3b00366d61b8e0bf8
Sinan NAR <sinan.nar@outlook.com>
2017-08-14 21:23:44 +1200

    BlockingCollection examples has been implemented

---
+++ Listing-1-29/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_1_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+
+            Task read = Task.Run(() =>
+            {
+                foreach(string v in col.GetConsumingEnumerable())
+                {
+                    Console.WriteLine("TAKEN=>" + v);
+                }
+            });
+
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s))
+                        break;
+                    else
+                        col.Add(s);
+                }
+            });
+            write.Wait();
+        }
+    }
+}
